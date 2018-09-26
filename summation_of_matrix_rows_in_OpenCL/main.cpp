75176c4d2c178612b6689ca6259841a0eefc55f2
savoptik <sav70792@gmail.com>
2018-09-26 10:33:29 +0300

    Initial Commit

---
+++ summation_of_matrix_rows_in_OpenCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  summation_of_matrix_rows_in_OpenCL
+//
+//  Created by Артём Семёнов on 26/09/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
